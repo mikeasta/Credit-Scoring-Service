a373d47c55b43e77d3548017e7c5920f26dc2812
mikeasta <m.astashonak@gmail.com>
2026-01-14 18:32:25 +0300

    Backend server code documented and optimized

--- app/backend/app.py
+++ app/backend/app.py
@@ -3,10 +3,15 @@ import pandas as pd
 from pathlib import Path
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Literal, Any
 from database import get_record_by_name
-from catboost import CatBoostClassifier
+from utils import (
+    get_classifier, 
+    process_client_data, 
+    predict_default,
+    create_result_message
+)
 
+# FastAPI Setup
 class ClientData(BaseModel):
     name_surname: str
     age: int
@@ -19,109 +24,14 @@ class ClientData(BaseModel):
 app = FastAPI()
 
 
-def get_classifier(client_type: str | Literal["new_client", "old_client"]) -> Any:
-    # Get path relative to this file's directory
-    base_dir = Path(__file__).parent
-    model_path = None
-    match(client_type):
-        case "new_client": 
-            model_path = base_dir / "models" / "best_catboost_new_client_model_v1.cbm"
-        case "old_client": 
-            model_path = base_dir / "models" / "best_catboost_old_client_model_v1.cbm"
-        case _: 
-            raise Exception(f"There is no such a client type: {client_type}")
-
-    model = CatBoostClassifier()
-    model.load_model(str(model_path))
-    return model
-
-
-def process_client_data(
-    form_data: dict | pd.DataFrame, 
-    client_data: dict | pd.DataFrame = None
-):
-    """Processes client data and returns features input"""
-    features = []
-    if client_data is None:
-        features = [
-            form_data.age,
-            form_data.education,
-            form_data.income,
-            form_data.has_car,
-            form_data.has_work,
-            form_data.has_passport
-        ]
-    else:
-        client_data = client_data.iloc[0].to_dict()
-        features = [
-            form_data.age,
-            form_data.education,
-            form_data.income,
-            form_data.has_car,
-            client_data["car_is_foreign"] if form_data.has_car else 0,
-            form_data.has_work,
-            form_data.has_passport,
-            client_data["bki_score"],
-            client_data["requests_count"],
-            client_data["rejected_applications_count"],
-            client_data["region_rating"],
-            client_data["home_address_category"],
-            client_data["work_address_category"],
-            client_data["social_network_analysis_score"],
-            client_data["first_record_age"],
-        ]
-
-    return features
-    
-
-def load_yaml_config(path: str | Path) -> dict:
-    """Loads YAML config as Python dictionary"""
-    with open(path, "r") as stream:
-        config = yaml.safe_load(stream)
-        return config
-
-
-def predict_default(
-    default_probability_estimation: float,
-    client_present: bool
-):
-    """Predicts client default"""
-    # Load model thresholds
-    base_dir = Path(__file__).parent
-    config_path = base_dir / "server.yaml"
-    classifier_config = load_yaml_config(config_path)["classifier"]
-    threshold_type = "old_client_threshold" if client_present else "new_client_threshold"
-    threshold = classifier_config[threshold_type]
-    return (default_probability_estimation > threshold)
-
-
-def create_result_message(
-    client_present: bool,
-    approved: bool,
-    name_surname: str = None
-) -> str:
-    """Creates scoring desicion message"""
-    # Choose greet variant
-    greet = ""
-    if client_present and name_surname:
-        greet = f"С возвращением, {name_surname}."
-    elif not client_present and name_surname:
-        greet = f"Добро пожаловать, {name_surname}!"
-    else:
-        greet = "Добро пожаловать"
-
-    # Choose decision variant
-    decision = ""
-    if approved:
-        decision = "Ваша заявка одобрена"
-    else:
-        decision = "Мы подобрали для вас альтернативный вариант - карта с 5% кэшбеком во всех супермаркетах Санкт-Петербурга"
-
-    return " ".join([greet, decision])
-
+# === ENDPOINTS ===
 
 @app.post("/score")
 def score(data: ClientData):
+    """
+    Scores client according to his info and returns credit-scoring system decision result
+    """
+
     # Check if client in database
     client_data = get_record_by_name(data.name_surname)
    
@@ -132,7 +42,7 @@ def score(data: ClientData):
         client_type="old_client" if client_present else "new_client"
     )
 
-    # Prepare input data
+    # Prepare input data to suit model input
     features_input = process_client_data(
         form_data=data, 
         client_data=client_data if client_present else None
@@ -141,18 +51,21 @@ def score(data: ClientData):
     # Make prediction
     default_proba = classifier.predict_proba(features_input)[1]
     default = predict_default(default_proba, client_present)
-    approved = not default
 
     # Create result message
     message = create_result_message(
         client_present=client_present,
-        approved=approved,
+        approved=not default,
         name_surname=data.name_surname
     )
 
     return { "message": message }
 
+
 @app.get("/greet")
 def greet():
+    """
+    Test message for fast and convenient troubleshooting
+    """
     return { "message": "Hello World!" }
     
